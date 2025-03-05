c6a55075a12700d24e227f98c2f4e9a08cca577a
Sjors van Holst <sjorsvanholst@gmail.com>
2025-03-05 11:49:57 +0100

    ♻️ Implemented image generation

--- next/app/api/handle-ingredient-database/route.ts
+++ next/app/api/handle-ingredient-database/route.ts
@@ -2,46 +2,92 @@ import { handleError } from "@/helper";
 import { removeBackground } from "@/utils/background";
 import { generateIcon, normalizeTitle } from "@/utils/openai";
 import supabase from "@/utils/supabase";
+import { after } from "next/server";
 
 export async function POST(request: Request) {
   const body = await request.json();
-  console.log(body);
-  // const uuid = body.record.console.log(body);
 
-  return new Response("{}", { status: 200 });
+  const ingredientUuid = body.record.uuid;
+  const ingredientTitle = body.record.title;
 
-  const title = await normalizeTitle("BigMac");
+  // If the title hasn't yet been generated we'll skip
+  if (!ingredientTitle) {
+    return new Response("{}", { status: 200 });
+  }
 
-  console.log(`[Icon] Looking up ${title}`);
+  after(async () => {
+    // Normalize the title and look it up in the database
+    console.log(`[ICON] Normalizing title`);
+    const iconTitle = await normalizeTitle(ingredientTitle);
+    const iconUuid = await fetchIcon(iconTitle);
 
-  const { error: fetchError, data: fetchData } = await supabase
+    // If the icon already exists we'll update the ingredient
+    if (iconUuid) {
+      await updateIngredient(ingredientUuid, iconUuid);
+
+      return new Response("{}", { status: 200 });
+    }
+
+    const imagePipeline = async () => {
+      console.log(`[ICON] Generating icon using AI`);
+      const base64 = await generateIcon(iconTitle);
+
+      console.log(`[ICON] Removing background from icon`);
+      const blob = await removeBackground(base64);
+
+      return blob;
+    };
+
+    const databasePipeline = async () => {
+      const insertUuid = await insertIcon(iconTitle);
+
+      return insertUuid;
+    };
+
+    // We'll run both pipelines in parallel
+    const [blob, uuid] = await Promise.all([
+      imagePipeline(),
+      databasePipeline(),
+    ]);
+
+    // We'll upload the icon and update the ingredient
+    await uploadIcon(uuid, blob);
+    await updateIngredient(ingredientUuid, uuid);
+
+    return new Response("{}", { status: 200 });
+  });
+}
+
+const updateIngredient = async (ingredient: string, icon: string) => {
+  console.log(`[ICON] Updating ingredient with icon`);
+
+  const { error } = await supabase
+    .from("ingredient")
+    .update({ icon_id: icon })
+    .eq("uuid", ingredient);
+
+  handleError(error);
+};
+
+const fetchIcon = async (title: string) => {
+  console.log(`[ICON] Fetching icon from database`);
+
+  const { error, data } = await supabase
     .from("icon")
     .select("uuid")
     .eq("title", title);
 
-  handleError(fetchError);
+  handleError(error);
 
-  const fetchSafe = fetchData || [];
-  const fetchUuid = fetchSafe[0]?.uuid;
+  // We can't use single since it will throw an error if the data is empty
+  const icons = data || [];
+  return icons[0]?.uuid;
+};
 
-  if (fetchUuid) {
-    return;
-  }
+const insertIcon = async (title: string) => {
+  console.log(`[ICON] Inserting icon into database`);
 
-  console.log(`[Icon] Generating ${title}...`);
-
-  const base64 = await generateIcon(title);
-
-  console.log(`[Icon] Removing background...`);
-
-  const blob = await removeBackground(base64);
-
-  console.log(`[Icon] Inserting icon into database`);
-
-  // const content = decode(base64);
-  const contentType = "image/jpeg";
-
-  const { error: iconError, data: iconData } = await supabase
+  const { error, data } = await supabase
     .from("icon")
     .insert({
       title,
@@ -49,17 +95,17 @@ export async function POST(request: Request) {
     .select("uuid")
     .single();
 
-  handleError(iconError);
+  handleError(error);
 
-  const insertUuid = iconData!.uuid;
+  return data!.uuid as string;
+};
 
-  console.log(`[Icon] Uploading icon to storage`);
+const uploadIcon = async (uuid: string, blob: Blob) => {
+  console.log(`[ICON] Uploading icon`);
 
-  const { error: bucketError } = await supabase.storage
-    .from("icon")
-    .upload(insertUuid, blob, {
-      contentType,
-    });
+  const { error } = await supabase.storage.from("icon").upload(uuid, blob, {
+    contentType: "image/png",
+  });
 
-  handleError(bucketError);
-}
+  handleError(error);
+};
