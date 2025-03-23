96c0758568239533842e244c870be621cdc29e87
Sjors van Holst <sjorsvanholst@gmail.com>
2025-03-23 22:56:55 +0100

    ✨ Added search endpoint

--- next/app/api/openfood/route.ts
+++ next/app/api/openfood/route.ts
@@ -6,17 +6,52 @@ export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
 
   const code = searchParams.get("code");
+  const query = searchParams.get("query");
+
   const lang = searchParams.get("lang");
   const limit = searchParams.get("limit");
   const offset = searchParams.get("offset");
 
-  if (!code) {
-    return NextResponse.json({ error: "Code is required" }, { status: 400 });
+  // https://search.openfoodfacts.org/search?q=coca cola&langs=nl&page_size=10&page=1
+
+  // If no code or query
+  if (!code && !query) {
+    return NextResponse.json(
+      { error: "Code or query is required" },
+      { status: 400 }
+    );
+  }
+
+  // If both code and query are provided
+  if (code && query) {
+    return NextResponse.json(
+      { error: "Only one of code or query is allowed" },
+      { status: 400 }
+    );
+  }
+
+  if (query) {
+    const params = new URLSearchParams();
+    const base = `https://search.openfoodfacts.org/search`;
+
+    if (lang) params.append("langs", lang);
+    if (limit) params.append("page_size", limit);
+    if (offset) params.append("page", offset);
+
+    params.append("q", query);
+
+    const url = `${base}?${params.toString()}`;
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const products = data.hits;
+    const nutritions = products.map(mapNutrition);
+
+    return NextResponse.json(nutritions);
   }
 
   const url = `https://world.openfoodfacts.org/api/v3/product/${code}.json`;
   const response = await fetch(url);
-
   const data = await response.json();
 
   if (!data.product) {
@@ -24,8 +59,13 @@ export async function GET(request: Request) {
   }
 
   const product = data.product;
-  const nutriments = product.nutriments;
+  const nutrition = mapNutrition(product);
 
+  return NextResponse.json(nutrition);
+}
+
+const mapNutrition = (product: any): IngredientInsert => {
+  const nutriments = product.nutriments;
   const nutritionPortion = roundNumber(+nutriments.fat_100g);
 
   const nutritionFats = roundNumber(nutriments.fat_100g ?? 0);
@@ -35,7 +75,7 @@ export async function GET(request: Request) {
     nutritionFats - nutritionSaturated - nutritionTrans
   );
 
-  const nutrition = {
+  return {
     type: "openfood",
     title: product.product_name,
     brand: product.brands,
@@ -60,7 +100,5 @@ export async function GET(request: Request) {
     fat_saturated_100g: nutritionSaturated,
     fat_unsaturated_100g: nutritionUnsaturated,
     micros_100g: {},
-  } as IngredientInsert;
-
-  return NextResponse.json(nutrition);
-}
+  };
+};
