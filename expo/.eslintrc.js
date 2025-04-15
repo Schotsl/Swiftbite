44bf28ecd2d28d2a301efe7183df5b79f299feb0
Sjors van Holst <sjorsvanholst@gmail.com>
2025-04-15 10:12:11 +0200

    ♻️ Fixed linter issues

--- expo/.eslintrc.js
+++ expo/.eslintrc.js
@@ -1,9 +1,16 @@
-module.exports = {
-  extends: "expo",
-  plugins: ["simple-import-sort", "prettier"],
-  rules: {
-    "simple-import-sort/imports": "error",
-    "simple-import-sort/exports": "error",
-    "prettier/prettier": "error",
+const { defineConfig } = require('eslint/config');
+const expoConfig = require('eslint-config-expo/flat');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+
+module.exports = defineConfig([
+  expoConfig,
+  eslintPluginPrettierRecommended,
+  {
+    ignores: ['dist/*'],
   },
-};
+  {
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+]);
