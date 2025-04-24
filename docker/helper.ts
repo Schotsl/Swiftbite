7c44b4d7a843e096c67e3fb1701d18bb50f00449
Sjors van Holst <sjorsvanholst@gmail.com>
2025-04-24 21:48:12 +0200

    🔥 Removed docker

--- docker/helper.ts
+++
@@ -1,7 +0,0 @@
-export const handleError = (error: Error | null) => {
-  if (error) {
-    console.log(error);
-
-    throw new Error(error.message);
-  }
-};
--- docker/load.py
+++
@@ -1,8 +0,0 @@
-from backgroundremover.bg import remove;
-
-# This file is called from Docker to load the models used in the backgroundremover.bg module. It reads the image file and sends it to the r
-file = open('load.png', 'rb');
-data = file.read();
-file.close();
-
-remove(data)
