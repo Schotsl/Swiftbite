4a920237940b29d53f7b4f77775a0fe2add8a034
Sjors van Holst <sjorsvanholst@gmail.com>
2025-04-29 13:43:13 +0200

    ✨ "Added" exports page

--- expo/app/(tabs)/personal/export-data.tsx
+++ expo/app/(tabs)/personal/export-data.tsx
@@ -1,13 +1,26 @@
+import Button from "@/components/Button";
 import Header from "@/components/Header";
 
-import { View } from "react-native";
+import { Alert, View } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 
 export default function ExportData() {
+  const handleExport = () => {
+    Alert.alert(
+      "Dit is helaas nog niet mogelijk",
+      "De exporteer functie is onderweg, maar nog niet klaar. We werken hard aan deze en andere functionaliteiten."
+    );
+  };
+
   return (
     <ScrollView>
-      <View style={{ gap: 48, flex: 1, padding: 32 }}>
-        <Header title="Exporteer gegevens" />
+      <View style={{ flex: 1, padding: 32 }}>
+        <Header
+          title="Exporteer gegevens"
+          content="Klik op 'Exporteer gegevens' en ontvang binnen enkele minuten een e-mail met een CSV-bestand van al je voedingsdata."
+        />
+
+        <Button title="Exporteer gegevens" onPress={handleExport} />
       </View>
     </ScrollView>
   );
