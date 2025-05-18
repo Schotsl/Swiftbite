d277d4687cdc077465b1747f13bc7744962b7eeb
Sjors van Holst <sjorsvanholst@gmail.com>
2025-05-18 12:42:00 +0200

    🐛 Fixed repeat menu

--- expo/app/(tabs)/automations/repeat/upsert/product/index.tsx
+++ expo/app/(tabs)/automations/repeat/upsert/product/index.tsx
@@ -1,13 +1,52 @@
 import PageProduct from "@/components/Page/Product";
 
-import { router } from "expo-router";
-import { useEditRepeat } from "@/context/RepeatContext";
+import { View } from "react-native";
+import { useQuery } from "@tanstack/react-query";
 import { ServingData } from "@/schemas/serving";
+import { useEditRepeat } from "@/context/RepeatContext";
+import { Redirect, router, useLocalSearchParams } from "expo-router";
+
+import productData from "@/queries/productData";
+import HeaderLoading from "@/components/Header/Loading";
+import ProductStatus from "@/components/Product/Status";
 
 export default function AutomationRepeatUpsertProduct() {
-  const { product, serving, removeProduct, updateServing } = useEditRepeat();
+  const {
+    product: productEditing,
+    serving,
+    updateProduct,
+    removeProduct,
+    updateServing,
+  } = useEditRepeat();
+
+  const { product: productId } = useLocalSearchParams<{
+    product: string;
+  }>();
+
+  const { data: productSearch, isLoading: isLoadingSearch } = useQuery({
+    ...productData({ uuid: productId }),
+    select: (products) => products[0],
+    enabled: !productEditing,
+  });
+
+  if (isLoadingSearch) {
+    return (
+      <View style={{ padding: 32, minHeight: "100%" }}>
+        <HeaderLoading />
+
+        <ProductStatus status="We zijn het product in onze database aan het zoeken" />
+      </View>
+    );
+  }
+
+  const product = productEditing || productSearch;
+
+  if (!product) {
+    return <Redirect href="/(tabs)/automations/repeat/upsert" />;
+  }
 
   const handleSave = async (returnedServing: ServingData) => {
+    updateProduct(product);
     updateServing(returnedServing);
 
     router.replace("/(tabs)/automations/repeat/upsert");
@@ -21,7 +60,7 @@ export default function AutomationRepeatUpsertProduct() {
 
   return (
     <PageProduct
-      product={product!}
+      product={productEditing || productSearch!}
       serving={serving}
       onSave={handleSave}
       onDelete={handleDelete}
