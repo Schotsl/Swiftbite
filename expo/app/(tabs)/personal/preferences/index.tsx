ec3bf4db923377b9a0dd5749751a19ec9b487ca9
Sjors van Holst <sjorsvanholst@gmail.com>
2025-05-21 12:38:06 +0200

    ♻️ Refactored preferences page

--- expo/app/(tabs)/personal/preferences/index.tsx
+++ expo/app/(tabs)/personal/preferences/index.tsx
@@ -3,15 +3,16 @@ import Header from "@/components/Header";
 import InputLabel from "@/components/Input/Label";
 import InputDropdown from "@/components/Input/Dropdown";
 import InputDropdownRadio from "@/components/Input/Dropdown/Radio";
+import ProductStatus from "@/components/Product/Status";
 
 import useUpdateUser from "@/mutations/useUpdateUser";
 import userData from "@/queries/userData";
 
+import { User } from "@/types/user";
 import { View } from "react-native";
 import { useForm } from "react-hook-form";
-import { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
-import { useRouter } from "expo-router";
+import { Suspense } from "react";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { ScrollView } from "react-native-gesture-handler";
 import { zodResolver } from "@hookform/resolvers/zod";
 
@@ -44,14 +45,39 @@ const languages = [
 ];
 
 export default function PersonalPreferences() {
-  const router = useRouter();
+  const { data: user } = useSuspenseQuery(userData());
 
+  return (
+    <ScrollView>
+      <View style={{ flex: 1, padding: 32 }}>
+        <Header title="Mijn voorkeur" />
+
+        <Suspense fallback={<PersonalPreferencesLoading />}>
+          <PersonalPreferencesForm user={user} />
+        </Suspense>
+      </View>
+    </ScrollView>
+  );
+}
+
+function PersonalPreferencesLoading() {
+  return <ProductStatus status="We zijn je voorkeuren aan het laden..." />;
+}
+
+type PersonalPreferencesFormProps = {
+  user: User;
+};
+
+function PersonalPreferencesForm({ user }: PersonalPreferencesFormProps) {
   const updateUser = useUpdateUser();
 
-  const [isLoading, setIsLoading] = useState(false);
-
-  const { data: user } = useQuery(userData());
-  const { control, handleSubmit, setValue, watch } = useForm<PreferenceData>({
+  const {
+    control,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { isSubmitting },
+  } = useForm<PreferenceData>({
     resolver: zodResolver(preferenceSchema),
     defaultValues: user,
   });
@@ -59,76 +85,56 @@ export default function PersonalPreferences() {
   const measurement = watch("measurement");
 
   const handleSave = (data: PreferenceData) => {
-    if (!user) {
-      return;
-    }
-
-    if (isLoading) {
-      return;
-    }
-
-    setIsLoading(true);
-
     updateUser.mutate({ ...user, ...data });
-
-    router.replace("/(tabs)/personal");
-
-    setIsLoading(false);
   };
 
   return (
-    <ScrollView>
-      <View style={{ flex: 1, padding: 32 }}>
-        <Header title="Mijn voorkeur" />
+    <View style={{ gap: 48 }}>
+      <View style={{ gap: 32 }}>
+        <InputDropdown
+          name="language"
+          label="Taal"
+          control={control}
+          options={languages}
+          placeholder="Taal"
+        />
 
-        <View style={{ gap: 48 }}>
-          <View style={{ gap: 32 }}>
-            <InputDropdown
-              name="language"
-              label="Taal"
-              control={control}
-              options={languages}
-              placeholder="Taal"
-            />
+        <View>
+          <InputLabel label="Meetsysteem" />
 
-            <View>
-              <InputLabel label="Meetsysteem" />
+          <InputDropdownRadio
+            style={{
+              borderBottomLeftRadius: 0,
+              borderBottomRightRadius: 0,
+            }}
+            label="Metrisch systeem"
+            selected={measurement === "metric"}
+            onSelect={() => {
+              setValue("measurement", "metric");
+            }}
+          />
 
-              <InputDropdownRadio
-                style={{
-                  borderBottomLeftRadius: 0,
-                  borderBottomRightRadius: 0,
-                }}
-                label="Metrisch systeem"
-                selected={measurement === "metric"}
-                onSelect={() => {
-                  setValue("measurement", "metric");
-                }}
-              />
-
-              <InputDropdownRadio
-                style={{
-                  marginTop: -2,
-                  borderTopLeftRadius: 0,
-                  borderTopRightRadius: 0,
-                }}
-                label="Imperial systeem"
-                selected={measurement === "imperial"}
-                onSelect={() => {
-                  setValue("measurement", "imperial");
-                }}
-              />
-            </View>
-          </View>
-
-          <Button
-            title="Wijzigen opslaan"
-            onPress={handleSubmit(handleSave)}
-            loading={isLoading}
-            disabled={isLoading}
+          <InputDropdownRadio
+            style={{
+              marginTop: -2,
+              borderTopLeftRadius: 0,
+              borderTopRightRadius: 0,
+            }}
+            label="Imperial systeem"
+            selected={measurement === "imperial"}
+            onSelect={() => {
+              setValue("measurement", "imperial");
+            }}
           />
         </View>
       </View>
-    </ScrollView>
+
+      <Button
+        title="Wijzigen opslaan"
+        onPress={handleSubmit(handleSave)}
+        loading={isSubmitting}
+        disabled={isSubmitting}
+      />
+    </View>
   );
 }
