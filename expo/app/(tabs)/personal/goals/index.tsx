d57f25980db152308e0d08974f6bc6de023d6318
Sjors van Holst <sjorsvanholst@gmail.com>
2025-05-21 12:40:55 +0200

    ♻️ Cleaned goals page

--- expo/app/(tabs)/personal/goals/index.tsx
+++ expo/app/(tabs)/personal/goals/index.tsx
@@ -2,75 +2,80 @@ import Button from "@/components/Button";
 import Header from "@/components/Header";
 import InputMacro from "@/components/Input/Macro";
 import InputCalorie from "@/components/Input/Calorie";
+import ProductStatus from "@/components/Product/Status";
 
 import useUpdateUser from "@/mutations/useUpdateUser";
 import userData from "@/queries/userData";
 
+import { User } from "@/types/user";
 import { View } from "react-native";
 import { useForm } from "react-hook-form";
-import { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
-import { useRouter } from "expo-router";
+import { Suspense } from "react";
 import { ScrollView } from "react-native-gesture-handler";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { GoalData, goalSchema } from "@/schemas/personal/goal";
 
 export default function PersonalGoals() {
-  const router = useRouter();
-
-  const updateUser = useUpdateUser();
-
-  const [isLoading, setIsLoading] = useState(false);
-
-  const { data: user } = useQuery(userData());
-  const { control, watch, handleSubmit } = useForm<GoalData>({
-    resolver: zodResolver(goalSchema),
-    defaultValues: user,
-  });
-
-  const handleSave = (data: GoalData) => {
-    if (!user) {
-      return;
-    }
-
-    if (isLoading) {
-      return;
-    }
-
-    setIsLoading(true);
-
-    updateUser.mutate({ ...user, ...data });
-
-    router.replace("/(tabs)/personal");
-
-    setIsLoading(false);
-  };
-
-  const calories = watch("calories");
+  const { data: user } = useSuspenseQuery(userData());
 
   return (
     <ScrollView>
       <View style={{ flex: 1, padding: 32 }}>
         <Header title="Verander je doel" />
 
-        <View style={{ gap: 48 }}>
-          <InputCalorie name="calories" label="Calorie" control={control} />
-
-          <InputMacro
-            name="macro"
-            label="Je macronutriënten"
-            control={control}
-            calories={calories}
-          />
-
-          <Button
-            title="Wijzigen opslaan"
-            onPress={handleSubmit(handleSave)}
-            loading={isLoading}
-            disabled={isLoading}
-          />
-        </View>
+        <Suspense fallback={<PersonalGoalsLoading />}>
+          <PersonalGoalsForm user={user} />
+        </Suspense>
       </View>
     </ScrollView>
+  );
+}
+
+function PersonalGoalsLoading() {
+  return <ProductStatus status="We zijn je doelen aan het laden..." />;
+}
+
+type PersonalGoalsFormProps = {
+  user: User;
+};
+
+function PersonalGoalsForm({ user }: PersonalGoalsFormProps) {
+  const updateUser = useUpdateUser();
+
+  const {
+    watch,
+    handleSubmit,
+    control,
+    formState: { isSubmitting },
+  } = useForm<GoalData>({
+    resolver: zodResolver(goalSchema),
+    defaultValues: user,
+  });
+
+  const handleSave = (data: GoalData) => {
+    updateUser.mutate({ ...user, ...data });
+  };
+
+  const calories = watch("calories");
+
+  return (
+    <View style={{ gap: 48 }}>
+      <InputCalorie name="calories" label="Calorie" control={control} />
+
+      <InputMacro
+        name="macro"
+        label="Je macronutriënten"
+        control={control}
+        calories={calories}
+      />
+
+      <Button
+        title="Wijzigen opslaan"
+        onPress={handleSubmit(handleSave)}
+        loading={isSubmitting}
+        disabled={isSubmitting}
+      />
+    </View>
   );
 }
