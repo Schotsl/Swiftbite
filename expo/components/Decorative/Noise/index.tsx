cdfc95219d5d43159512a7efc207547bbcd26247
Sjors van Holst <sjorsvanholst@gmail.com>
2025-05-27 17:02:00 +0200

    💄 Expanded noise tiles

--- expo/components/Decorative/Noise/index.tsx
+++ expo/components/Decorative/Noise/index.tsx
@@ -5,10 +5,27 @@ export default function DecorativeNoise() {
   return (
     <View
       style={{
+        width: 500,
+        height: 500,
+        position: "absolute",
+        flexWrap: "wrap",
+      }}
+    >
+      <DecorativeTile />
+      <DecorativeTile />
+      <DecorativeTile />
+      <DecorativeTile />
+    </View>
+  );
+}
+
+export function DecorativeTile() {
+  return (
+    <View
+      style={{
         width: 250,
         height: 250,
         opacity: 0.06,
-        position: "absolute",
       }}
     >
       <Image
