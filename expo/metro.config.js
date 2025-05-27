b9b07f728968219d7a0e70de08a3cf01d142e835
Sjors van Holst <sjorsvanholst@gmail.com>
2025-05-27 10:30:46 +0200

    ⬆️ Upgraded to Expo SDK 53

--- expo/metro.config.js
+++ expo/metro.config.js
@@ -2,4 +2,7 @@ const { getSentryExpoConfig } = require("@sentry/react-native/metro");
 
 const config = getSentryExpoConfig(__dirname);
 
+// This is a hotfix for Supabase but can be removed in a bit once the new release hits
+config.resolver.unstable_enablePackageExports = false;
+
 module.exports = config;
