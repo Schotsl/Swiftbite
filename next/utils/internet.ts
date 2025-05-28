0d3753e86cef35bc7b3aa41a80588bc389f1681d
Sjors van Holst <sjorsvanholst@gmail.com>
2025-05-28 09:49:12 +0200

    🐛 Debugging Openfoodfacts

--- next/utils/internet.ts
+++ next/utils/internet.ts
@@ -43,82 +43,89 @@ export const openfoodRequest = async (
   lang: string,
   signal: AbortSignal
 ) => {
-  const timeStart = performance.now();
+  try {
+    const timeStart = performance.now();
 
-  const url = "https://search.openfoodfacts.org/search";
-  const method = "POST";
-  const headers = { "Content-Type": "application/json" };
-  const body = JSON.stringify({
-    q: query,
-    lang: lang,
-    page: 1,
-    page_size: 32,
-    fields: [
-      "code",
-      "lang",
-      "brands",
-      "brands_tags",
-      "quantity",
-      "countries_tags",
-      "product_name",
-      "product_name_en",
-      "product_name_nl",
-      "product_name_fr",
-      "product_name_de",
-      "product_name_it",
-      "product_name_es",
-      "product_name_pt",
-      "product_name_ru",
-      "product_name_pl",
-      "product_name_tr",
-      "generic_name",
-      "generic_name_en",
-      "generic_name_nl",
-      "generic_name_fr",
-      "generic_name_de",
-      "generic_name_it",
-      "generic_name_es",
-      "generic_name_pt",
-      "generic_name_ru",
-      "generic_name_pl",
-      "generic_name_tr",
-    ],
-  });
+    const url = "https://search.openfoodfacts.org/search";
+    const method = "POST";
+    const headers = { "Content-Type": "application/json" };
+    const body = JSON.stringify({
+      q: query,
+      lang: lang,
+      page: 1,
+      page_size: 32,
+      fields: [
+        "code",
+        "lang",
+        "brands",
+        "brands_tags",
+        "quantity",
+        "countries_tags",
+        "product_name",
+        "product_name_en",
+        "product_name_nl",
+        "product_name_fr",
+        "product_name_de",
+        "product_name_it",
+        "product_name_es",
+        "product_name_pt",
+        "product_name_ru",
+        "product_name_pl",
+        "product_name_tr",
+        "generic_name",
+        "generic_name_en",
+        "generic_name_nl",
+        "generic_name_fr",
+        "generic_name_de",
+        "generic_name_it",
+        "generic_name_es",
+        "generic_name_pt",
+        "generic_name_ru",
+        "generic_name_pl",
+        "generic_name_tr",
+      ],
+    });
 
-  const response = await fetch(url, {
-    body,
-    signal,
-    method,
-    headers,
-  });
+    const response = await fetch(url, {
+      body,
+      signal,
+      method,
+      headers,
+    });
 
-  const parsed = await response.json();
-  const results = parsed.hits;
+    const parsed = await response.json();
+    const results = parsed.hits;
 
-  // Remove duplicates brand labels from openfoodItems
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const minimized = results.map((item: any) => {
-    const brands = item.brands || [];
-    const brandsTags = item.brands_tags || [];
-    const brandsCombined = [...brands, ...brandsTags];
-    const brandsUnique = brandsCombined.filter(
-      (brand, index, self) =>
-        index === self.findIndex((t) => t.toLowerCase() === brand.toLowerCase())
-    );
+    // Remove duplicates brand labels from openfoodItems
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const minimized = results.map((item: any) => {
+      const brands = item.brands || [];
+      const brandsTags = item.brands_tags || [];
+      const brandsCombined = [...brands, ...brandsTags];
+      const brandsUnique = brandsCombined.filter(
+        (brand, index, self) =>
+          index ===
+          self.findIndex((t) => t.toLowerCase() === brand.toLowerCase())
+      );
 
-    delete item.brands_tags;
-    delete item.brands;
+      delete item.brands_tags;
+      delete item.brands;
 
-    return { ...item, brands: brandsUnique };
-  });
+      return { ...item, brands: brandsUnique };
+    });
 
-  const timeEnd = performance.now();
-  const timeDiff = Math.round(timeEnd - timeStart);
+    const timeEnd = performance.now();
+    const timeDiff = Math.round(timeEnd - timeStart);
 
-  console.log(`[SEARCH] Openfood request took ${timeDiff}ms`);
+    console.log(`[SEARCH] Openfood request took ${timeDiff}ms`);
 
-  const minimizedSafe = minimized || [];
-  return minimizedSafe;
+    const minimizedSafe = minimized || [];
+    return minimizedSafe;
+  } catch (error) {
+    console.error("[SEARCH] Openfood request error:", error);
+    throw error;
+    // return [];
+  }
 };
 
 export const supabaseRequest = async (
