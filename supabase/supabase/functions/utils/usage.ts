4db53788a5476d27de6d7eb6c7a2ca32b774f6d4
Sjors van Holst <sjorsvanholst@gmail.com>
2025-06-05 11:07:40 +0200

    🐛 Changes usage limit

--- supabase/supabase/functions/utils/usage.ts
+++ supabase/supabase/functions/utils/usage.ts
@@ -76,7 +76,7 @@ export async function validateUsage(user: string) {
     return "Hourly limit exceeded";
   }
 
-  if (yearInput > 500000 || yearOutput > 500000) {
+  if (yearInput > 5000000 || yearOutput > 5000000) {
     return "Yearly limit exceeded";
   }
 }
