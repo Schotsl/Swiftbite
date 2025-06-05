baf4888186d520baf3e82a1345e2823012f415e0
Sjors van Holst <sjorsvanholst@gmail.com>
2025-06-05 14:02:52 +0200

    💩 Added fake apple confirm

--- expo/components/Page/Stats/Charts/History/index.tsx
+++ expo/components/Page/Stats/Charts/History/index.tsx
@@ -5,6 +5,8 @@ import { useWindowDimensions, View } from "react-native";
 
 import TextSmall from "@/components/Text/Small";
 import TextLarge from "@/components/Text/Large";
+import TextBody from "@/components/Text/Body";
+import TextTitle from "@/components/Text/Title";
 
 import PageStatsChartsHistoryCalories from "./Calories";
 import PageStatsChartsHistoryBalance from "./Balance";
@@ -13,7 +15,7 @@ import PageStatsChartsHistoryWeight from "./Weight";
 import PageStatsSection from "@/components/Page/Stats/Section";
 
 import InputRange from "@/components/Input/Range";
-import TextTitle from "@/components/Text/Title";
+import ButtonSmall from "@/components/Button/Small";
 
 import variables from "@/variables";
 import language from "@/language";
@@ -26,6 +28,8 @@ export default function PageStatsChartsHistory({
   control,
 }: PageStatsChartsHistoryProps) {
   const [open, setOpen] = useState<string | null>("calories");
+
+  const [apple, setApple] = useState(true);
 
   const { width: widthWindow } = useWindowDimensions();
 
@@ -51,25 +55,35 @@ export default function PageStatsChartsHistory({
           onToggle={() => handleToggle("calories")}
         >
           <View style={{ gap: 8 }}>
-            <TextLarge weight="semibold">
-              {language.page.stats.charts.history.calories.calories.title}
-            </TextLarge>
-            <TextSmall>
-              {language.page.stats.charts.history.calories.calories.content}
-            </TextSmall>
+            <View style={{ position: "relative" }}>
+              <View style={{ opacity: apple ? 0.04 : 1, gap: 8 }}>
+                <TextLarge weight="semibold">
+                  {language.page.stats.charts.history.calories.calories.title}
+                </TextLarge>
+                <TextSmall>
+                  {language.page.stats.charts.history.calories.calories.content}
+                </TextSmall>
 
-            <PageStatsChartsHistoryCalories
-              width={width}
-              input={[
-                { consumed: 2300, burned: 2500 },
-                { consumed: 3100, burned: 3000 },
-                { consumed: 2700, burned: 2800 },
-                { consumed: 3000, burned: 3200 },
-                { consumed: 2800, burned: 2700 },
-                { consumed: 3000, burned: 2900 },
-                { consumed: 2900, burned: 3100 },
-              ]}
-            />
+                <PageStatsChartsHistoryCalories
+                  width={width}
+                  input={[
+                    { consumed: 2300, burned: 2500 },
+                    { consumed: 3100, burned: 3000 },
+                    { consumed: 2700, burned: 2800 },
+                    { consumed: 3000, burned: 3200 },
+                    { consumed: 2800, burned: 2700 },
+                    { consumed: 3000, burned: 2900 },
+                    { consumed: 2900, burned: 3100 },
+                  ]}
+                />
+              </View>
+
+              {apple && (
+                <PageStatsChartsHistoryOverlay
+                  onPress={() => setApple(false)}
+                />
+              )}
+            </View>
           </View>
 
           <View style={{ gap: 8 }}>
@@ -130,28 +144,75 @@ export default function PageStatsChartsHistory({
           onToggle={() => handleToggle("weight")}
         >
           <View style={{ gap: 8 }}>
-            <TextLarge weight="semibold">
-              {language.page.stats.charts.history.weight.weight.title}
-            </TextLarge>
-            <TextSmall>
-              {language.page.stats.charts.history.weight.weight.content}
-            </TextSmall>
+            <View style={{ position: "relative" }}>
+              <View style={{ opacity: apple ? 0.04 : 1, gap: 8 }}>
+                <TextLarge weight="semibold">
+                  {language.page.stats.charts.history.weight.weight.title}
+                </TextLarge>
+                <TextSmall>
+                  {language.page.stats.charts.history.weight.weight.content}
+                </TextSmall>
 
-            <PageStatsChartsHistoryWeight
-              width={width}
-              input={[
-                { calories: 2200, weight: 70.5 },
-                { calories: 2350, weight: 70.3 },
-                { calories: 2100, weight: 70.4 },
-                { calories: 2400, weight: 70.1 },
-                { calories: 2250, weight: 70.2 },
-                { calories: 2300, weight: 70.0 },
-                { calories: 2150, weight: 69.9 },
-              ]}
-            />
+                <PageStatsChartsHistoryWeight
+                  width={width}
+                  input={[
+                    { calories: 2200, weight: 70.5 },
+                    { calories: 2350, weight: 70.3 },
+                    { calories: 2100, weight: 70.4 },
+                    { calories: 2400, weight: 70.1 },
+                    { calories: 2250, weight: 70.2 },
+                    { calories: 2300, weight: 70.0 },
+                    { calories: 2150, weight: 69.9 },
+                  ]}
+                />
+              </View>
+
+              {apple && (
+                <PageStatsChartsHistoryOverlay
+                  onPress={() => setApple(false)}
+                />
+              )}
+            </View>
           </View>
         </PageStatsSection>
       </View>
+    </View>
+  );
+}
+
+type PageStatsChartsHistoryOverlayProps = {
+  onPress: () => void;
+};
+
+function PageStatsChartsHistoryOverlay({
+  onPress,
+}: PageStatsChartsHistoryOverlayProps) {
+  return (
+    <View
+      style={{
+        gap: 24,
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        position: "absolute",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <TextBody weight="semibold" align="center" style={{ marginTop: -16 }}>
+        Stel de Apple-integratie in en we importeren je gewicht. Zo kunnen we
+        uitgebreidere grafieken tonen en feedback geven op je caloriebudget.
+      </TextBody>
+      <ButtonSmall
+        icon="apple"
+        title="Apple-integratie instellen"
+        style={{
+          marginLeft: "auto",
+          marginRight: "auto",
+        }}
+        onPress={onPress}
+      />
     </View>
   );
 }
